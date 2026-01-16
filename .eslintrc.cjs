21164c26f5e2f9afe1fc531fd6cf1b686634dec8
marmishaw14 <marmishaw@academic.rrc.ca>
2026-01-16 13:28:18 -0600

    config: added ESLint and Prettier config

---
+++ .eslintrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  env: { node: true, es2022: true },
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  rules: {},
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+};
